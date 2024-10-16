e5f5aa8a9444f174798ae353e39029044c511712
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-10-16 12:31:04 -0700

    commit

---
+++ android/app/src/main/kotlin/com/example/movimate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.movimate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movimate/colors.dart';
+import 'package:movimate/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MoviMate',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colours.scaffoldBgColor,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
