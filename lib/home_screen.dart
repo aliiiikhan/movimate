5e642a992c60cdc37c98b09c52e19cb6c7318077
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-10-17 15:11:05 -0700

    commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,6 +10,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Image.asset(
+          'assets/movimate.jpg',
+          fit: BoxFit.cover,
+          height: 70,
+          width: 70,
+          filterQuality: FilterQuality.high,
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
