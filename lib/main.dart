2bba7fc9a9270be53dc07fc109849e9f927a741d
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-10-23 22:22:31 -0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MoviMate ki bto jjhgggkjhh',
+      title: 'MoviMate',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colours.scaffoldBgColor,
       ),
